239b87e0f3dea0dbacb4c5f1dad0bb6cde56cfee
Erick Sanchez <erick94310@gmail.com>
2025-07-31 01:01:27 +0000

    finished js

--- src/app.js
+++ src/app.js
@@ -5,29 +5,79 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  const randomFace = ()=>{ const world = document.querySelector("#World");
- 
-  const myArray = ["A","2","3","4","5","6","7","8","9","10","J","Q","K",]
-  const randomDec = Math.random()
-  const randomLongNumber = randomDec * myArray.length
-  const randomNumNoDec= Math.floor(randomLongNumber)
-  world.innerText= randomLongNumber
-world.innerHTML = myArray[randomNumNoDec]}
- const button = document.querySelector("#button")
- button.addEventListener("click" randomFace)
-}; 
-  const sayHello= ()=>{
-    console.log ("hello!!!")
-  }
-  
+window.onload = function () {
+
+  const randomCard = () => {
+
+    const divFace = document.querySelector("#card");
+
+    const faceArray = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K",]
+
+    const randomFaceDec = Math.random()
+
+    const randomLongNumber = randomFaceDec * faceArray.length
+
+    const randomNumberNoDec = Math.floor(randomLongNumber)
+
+    divFace.innerHTML = faceArray[randomNumberNoDec]
 
 
-  const card = document.querySelector("#card");{
-  const myArray = ["♦", "♥", "♠", "♣"]
-  const randomDec = Math.random()
-  const randomLongNumber = randomDec * myArray.length
-  const randomNumNoDec= Math.floor(randomLongNumber)
-  card.innerText= randomLongNumber
-card.innerHTML = myArray[randomNumNoDec]
+    const divSuit = document.querySelectorAll(".suit");
+
+    const suitArray = ["♦", "♥", "♠", "♣"]
+
+    const randomSuitDec = Math.random()
+
+    const randomSuitLongNumber = randomSuitDec * suitArray.length
+
+    const randomNumNoDec = Math.floor(randomSuitLongNumber)
+
+    divSuit.forEach(
+      (colorDivElement) => {
+         if (suitArray[randomNumNoDec] == "♦") {
+      colorDivElement.style.color = "red"
+      console.log("diamond")
+    }
+    if (suitArray[randomNumNoDec] == "♥") {
+      colorDivElement.style.color = "red"
+    }
+    if (suitArray[randomNumNoDec] == "♠" || suitArray[randomNumNoDec] == "♣") {
+      colorDivElement.style.color = "black"
+    }
+
+    colorDivElement.innerHTML = suitArray[randomNumNoDec]
+        colorDivElement.style.backgroundColor = "blue"
+
+      }
+    )
+    
+    if (suitArray[randomNumNoDec] == "♦") {
+      divSuit.style.color = "red"
+      console.log("diamond")
+    }
+    if (suitArray[randomNumNoDec] == "♥") {
+      divSuit.style.color = "red"
+    }
+    if (suitArray[randomNumNoDec] == "♠" || suitArray[randomNumNoDec] == "♣") {
+      divSuit.style.color = "black"
+    }
+
+    divSuit.innerHTML = suitArray[randomNumNoDec]
   }
+
+
+
+  const myButton = document.querySelector(".button");
+  myButton.style.backgroundColor = "green"
+  myButton.addEventListener("click", randomCard)
+
+  const mySuit = document.querySelector("#suit");
+  mySuit.addEventListener("click", randomCard)
+};
+
+
+
+
+
+
+
